7915c15b1f26524e00a09dca0b70031d9f1f821d
Zsofenszki Kristóf <zsofenszki.kristof@gmail.com>
2023-07-26 15:37:52 +0200

    Activity feed szerkesztése. Vid: 3h 42m

--- base/models.py
+++ base/models.py
@@ -26,14 +26,15 @@ class Room(models.Model):
         return self.name
 
 
-
-
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         # The first 50 characters of the message
