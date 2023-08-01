81287400fb7036faae49d3974a195848faa1bbec
Zsofenszki Kristóf <zsofenszki.kristof@gmail.com>
2023-08-01 21:01:47 +0200

    topics mobil nézet Vid: 5h 47m

--- base/views.py
+++ base/views.py
@@ -69,7 +69,7 @@ def home(request):
                                 Q(description__icontains=q)
                                 )
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -191,3 +191,10 @@ def updateUser(request):
             form.save()
             return redirect('user-profile', pk=user.id)
     return render(request, 'base/update-user.html', context)
+
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    context = {'topics': topics}
+    return render(request, 'base/topics.html', context)
