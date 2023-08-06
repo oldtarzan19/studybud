988ee0723c54731af243113a86bdecc9e17b6039
Zsofenszki Kristóf <zsofenszki.kristof@gmail.com>
2023-08-06 20:28:10 +0200

    Video end.

--- base/views.py
+++ base/views.py
@@ -5,9 +5,8 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 # from django.contrib.auth.models import User || Video utolso részben lett kicserelve
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message, User
-from .forms import RoomForm, UserForm
+from .forms import RoomForm, UserForm, MyUserCreationForm
 
 # Create your views here.
 
@@ -20,15 +19,15 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username').lower()
+        email = request.POST.get('email').lower()
         password = request.POST.get('password')
 
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=email)
         except:
             messages.error(request, 'Username does not exist')
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(request, email=email, password=password)
 
         if user is not None:
             login(request, user)
@@ -45,10 +44,10 @@ def logoutUser(request):
 
 
 def registerPage(request):
-    form = UserCreationForm()
+    form = MyUserCreationForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = MyUserCreationForm(request.POST)
 
         if form.is_valid():
             user = form.save(commit=False)
@@ -186,11 +185,12 @@ def updateUser(request):
     form = UserForm(instance=user)
     context = {'form': form}
     if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)
+        form = UserForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             form.save()
             return redirect('user-profile', pk=user.id)
     return render(request, 'base/update-user.html', context)
+
 
 
 def topicsPage(request):
